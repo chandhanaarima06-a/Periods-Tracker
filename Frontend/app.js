f480327862234c651e5f2ac0735936eca9781c58
Chandhana pushpa <chanadhanaarima06@gmail.com>
2026-06-19 13:31:39 +0530

    Add input validation foe empty date selection

--- Frontend/app.js
+++ Frontend/app.js
@@ -3,6 +3,11 @@ let button=document.getElementById("logButton");
 button.addEventListener("click",function(){
     let dateInput=document.getElementById("periodDate");
     let selectedDate=dateInput.value;
+    if(selectedDate === ""){
+        let predictionText=document.getElementById("predictionText");
+        predictionText.textContent="Please select a date first!";
+        return;
+    }
     let startDate=new Date(selectedDate);
     let nextDate=new Date(startDate);
     nextDate.setDate(startDate.getDate()+28);
