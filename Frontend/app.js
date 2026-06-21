c85b65c67b21898a25b26833f6b19d9c152261f6
Chandhana pushpa <chanadhanaarima06@gmail.com>
2026-06-21 08:59:08 +0530

    Refactor prediction logic into a reusable code

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,16 +1,21 @@
 console.log("Dearme is running");
+ function predictNextPeriod(startDateString){
+    let startDate=new Date(startDateString);
+    let nextDate=new Date(startDate);
+    nextDate.setDate(startDate.getDate()+28);
+    return nextDate.toDateString();
+    }
 let button=document.getElementById("logButton");
 button.addEventListener("click",function(){
     let dateInput=document.getElementById("periodDate");
     let selectedDate=dateInput.value;
+    let predictionText=document.getElementById("predictionText");
     if(selectedDate === ""){
         let predictionText=document.getElementById("predictionText");
         predictionText.textContent="Please select a date first!";
         return;
     }
-    let startDate=new Date(selectedDate);
-    let nextDate=new Date(startDate);
-    nextDate.setDate(startDate.getDate()+28);
-    let predictionText=document.getElementById("predictionText");
-    predictionText.textContent= "Your next period is predicted on: "  + nextDate.toDateString();
+   
+    let result=predictNextPeriod(selectedDate);
+    predictionText.textContent= "Your next period is predicted on: "  + result;
 });
